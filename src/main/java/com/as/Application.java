40939ea49fb461d8995235ec6f8e1f92da17d5d2
tedpet5 <tedpet5@yahoo.com>
2026-02-08 20:51:05 -0500

    Initial Commit

---
+++ src/main/java/com/as/Application.java
@@ -0,0 +1,57 @@
+package com.as;
+
+import com.as.components.Main;
+import com.webobjects.appserver.WOSession;
+
+import er.extensions.appserver.ERXApplication;
+import er.extensions.appserver.navigation.ERXNavigationManager;
+import er.extensions.foundation.ERXPatcher;
+
+public class Application extends ERXApplication {
+	public static void main(String[] argv) {
+		ERXApplication.main(argv, Application.class);
+	}
+
+	public Application() {
+		ERXApplication.log.info("Welcome to " + name() + " !");
+		setDefaultRequestHandler(requestHandlerForKey(directActionRequestHandlerKey()));
+		setAllowsConcurrentRequestHandling(true);		
+	}
+	
+    @Override
+    public void finishInitialization() {
+    	super.finishInitialization();
+    	
+    	// Setup main navigation
+    	ERXNavigationManager.manager().configureNavigation();
+    	
+    }
+    
+	/**
+	 * Determines the {@link WOSession} class to instantiate.
+	 *
+	 * @see com.webobjects.appserver.WOApplication#_sessionClass()
+	 */
+
+	@Override
+
+	protected Class<? extends WOSession> _sessionClass() {
+		return Session.class;
+
+	}
+
+
+	/**
+	 * Installs patches, including ensuring that {@code Main} is correctly resolved at runtime.
+	 *
+	 * @see er.extensions.appserver.ERXApplication#installPatches()
+	 */
+
+	@Override
+
+	public void installPatches() {
+		super.installPatches();
+		ERXPatcher.setClassForName(Main.class, "Main");
+
+	}
+}
